4864ec265f8c32b2db5bcb2cdffd1c4900ed591b
ninawrona <ninawrona@gmail.com>
2023-05-16 07:57:53 +0200

    adding a protected endpoint

--- session10-express/server.js
+++ session10-express/server.js
@@ -21,6 +21,26 @@ app.get('/', (req, res)=>{
     res.send("Hi, it's a message from a server.")
 })
 
+//defining authentication middleware before the endpoint
+//that needs to use it
+
+function requireAuth(req, res, next){
+    const token = req.headers.authorization?.split(" ")[1]
+
+    if(!token) return res.status(401).send({error: "Unauthorized"})
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) =>{
+        if (err) return res.status(401).send({error: "Unauthorized"})
+        req.user = decoded
+        next()
+    })
+}
+
+//endpoint that is protected by JWT authentication middleware
+app.get("/protected", requireAuth, (req, res) => {
+    res.send("Hi, you are authorized to protected route.")
+})
+
 //middleware
 function logger(req, res, next){
     console.log(req.originalUrl)
